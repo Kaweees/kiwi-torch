7c69f4c197c1a7b9779df37431da88e9d3c4a1bb
Miguel Villa Floran <miguel.villafloran@gmail.com>
2024-10-22 16:42:26 -0700

    Operators

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,31 @@
 #include <stddef.h>
 #include <stdio.h>
 
-#define EXIT_SUCCESS 0
-#define EXIT_FAILURE 1
+#include <iostream>
+
+#include "../include/operators.hpp"
+
+bool more_than_4(float x) { return x > 4; }
 
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
-int main(int argc, char **argv) { printf("Hello World!\n"); }
+int main(int argc, char* argv[]) {
+  std::cout << "combine3 with add: " << combine3(add, 1, 3, 5) << std::endl;
+  std::cout << "combine3 with mul: " << combine3(mul, 1, 3, 5) << std::endl;
+
+  auto add3 = combine3(add);
+  std::cout << "add3: " << add3(1, 3, 5) << std::endl;
+
+  auto filter_for_more_than_4 = filter(more_than_4);
+  std::vector<float> numbers = {1, 10, 3, 5};
+  auto filtered = filter_for_more_than_4(numbers);
+
+  std::cout << "Filtered numbers: ";
+  for (float num : filtered) {
+    std::cout << num << " ";
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
