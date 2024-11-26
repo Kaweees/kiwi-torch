9cf6c2eaffe7aa71df6d3379c6d870e90423c5b4
Kaweees <Kaweees@users.noreply.github.com>
2024-11-26 08:12:08 +0000

    Code formatting and linting

--- include/kiwitorch/kiwitorch.hpp
+++ include/kiwitorch/kiwitorch.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
+#include <kiwitorch/core/module.hpp>
 #include <kiwitorch/core/operators.hpp>
 #include <kiwitorch/core/scalar.hpp>
-#include <kiwitorch/core/module.hpp>
 #include <kiwitorch/core/tensor.hpp>
-
